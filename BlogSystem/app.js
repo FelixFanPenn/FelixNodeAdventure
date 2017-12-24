96398341961618554bb02ab690a0ee415f28512e
FelixFanPenn <fankejin@seas.upenn.edu>
2017-12-23 21:47:47 -0500

    changed .gitignore and implemented layout

--- BlogSystem/app.js
+++ BlogSystem/app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var expressValidator = require('expressValidator');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
-var db = require('monk')('localhost/BlogSystem');
+var db = require('monk')('mongodb://felix:felix@ds137882.mlab.com:37882/blogsystem');
+var multer = require('multer');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,13 +20,55 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+app.use(multer({ dest: './public/image/uploads'}).any());
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+
+app.use(expressValidator({ 
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.')
+		var root = namespace.shift()
+		var formParam = root;
+
+	while (namespace.length) {
+		formParam += '[' + namespace.length + ']';
+	}
+	return {
+		param: formParam,
+		msg: msg,
+		value: value
+	};
+
+	}
+}));
+
+app.use(flash());
+app.use(function(req, res, next){
+	res.locals.messages = require('express-messages')(req, res);
+	next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+	req.db = db;
+	next();
+});
+
 
 app.use('/', index);
 app.use('/users', users);
