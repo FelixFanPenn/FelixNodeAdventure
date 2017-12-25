ac150cfe2494e7eb931be6b298bfc3aacc587d13
FelixFanPenn <fankejin@seas.upenn.edu>
2017-12-25 13:05:20 -0500

    add comment feature

--- BlogSystem/routes/posts.js
+++ BlogSystem/routes/posts.js
@@ -15,6 +15,21 @@ router.get('/add', function(req, res, next) {
 	});
 });
 
+
+// :id will be the param in the url
+
+// for some reason i cannot get findById to work. maybe an issue in npm mongo pkg
+router.get('/show/:id', function(req, res, next) {
+	var posts = db.get("posts");
+	console.log(req.params.id);
+
+	posts.find({"_id": req.params.id}, {}, function(err, posts) {
+		res.render('show', {
+			'post': posts[0]
+		});
+	});
+});
+
 router.post('/add', function(req, res, next) {
 
 	// get form info
@@ -65,6 +80,67 @@ router.post('/add', function(req, res, next) {
 		});
 	}
 });
+
+
+
+router.post('/addcomment', function(req, res, next) {
+
+	// get form info
+	var name = req.body.name;
+	var email = req.body.email;
+	var body = req.body.body;
+	var postid = req.body.postid;
+	var commentdate = new Date();
+
+	// validation
+	req.checkBody("name", "name field is required").notEmpty();
+	req.checkBody("email", "email field is required").notEmpty();
+	req.checkBody("email", "invalid email").isEmail();
+	req.checkBody("body", "body field is required").notEmpty();
+
+	// check errors
+	var errors = req.validationErrors();
+	if (errors) {
+		var posts = db.get('posts');
+		posts.findById(postid, function(err, post){
+			res.render('show', {
+				"errors": errors,
+				"post": post
+			});
+		});
+
+	} else {
+		var comment = {
+			"name": name,
+			"email": email,
+			"body": body,
+			"commentdate": commentdate
+		}
+
+		var posts = db.get("posts");
+
+		posts.update({
+				"_id": postid,
+			},
+			{
+				$push:{
+					"comments": comment
+				}
+			},
+			function(err, doc) {
+				if (err) {
+					throw err;	
+				} else {
+					req.flash('success', "Comment Added");
+					res.location('/posts/show/' + postid);
+					res.redirect('/posts/show/' + postid);
+				}
+			}
+		);
+	}
+});
+
+
 
 
 module.exports = router;
