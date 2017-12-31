0bc0b9d92a4bfd6de714734efe016e2859aff439
FelixFanPenn <fankejin@seas.upenn.edu>
2017-12-31 17:57:07 -0500

    started two apps

---
+++ CommunityEvents/config.js
@@ -0,0 +1,89 @@
+'use strict';
+const Confidence = require('confidence');
+const Dotenv = require('dotenv');
+
+
+Dotenv.config({ silent: true });
+
+const criteria = {
+    env: process.env.NODE_ENV
+};
+
+
+const config = {
+    $meta: 'This file configures the plot device.',
+    projectName: 'Community Event',
+    port: {
+        web: {
+            $filter: 'env',
+            test: 9000,
+            production: process.env.PORT,
+            $default: 8000
+        }
+    },
+    baseUrl: {
+        $filter: 'env',
+        $meta: 'values should not end in "/"',
+        production: 'https://getaqua.herokuapp.com',
+        $default: 'http://127.0.0.1:8000'
+    },
+    authAttempts: {
+        forIp: 50,
+        forIpAndUser: 7
+    },
+    cookieSecret: {
+        $filter: 'env',
+        production: process.env.COOKIE_SECRET,
+        $default: '!k3yb04rdK4tz~4qu4~k3yb04rdd0gz!'
+    },
+    hapiMongoModels: {
+        mongodb: {
+            uri: {
+                $filter: 'env',
+                production: process.env.MONGODB_URI,
+                test: process.env.MONGODB_URI,
+                $default: process.env.MONGODB_URI,
+                //test: 'mongodb://felix:felix@ds235807.mlab.com:35807/communityevent',
+                //$default: 'mongodb://felix:felix@ds235807.mlab.com:35807/communityevent'
+            }
+        },
+        autoIndex: true
+    },
+    nodemailer: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            //user: 'fankejin@gmail.com',
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD
+        }
+    },
+    system: {
+        fromAddress: {
+            name: 'Community Event',
+            //address: 'fankejin@gmail.com'
+            address: process.env.APP_SUPPORT_EMAIL
+        },
+        toAddress: {
+            name: 'Community Event',
+            //address: 'fankejin@gmail.com'
+            address: process.env.APP_SUPPORT_EMAIL
+        }
+    }
+};
+
+
+const store = new Confidence.Store(config);
+
+
+exports.get = function (key) {
+
+    return store.get(key, criteria);
+};
+
+
+exports.meta = function (key) {
+
+    return store.meta(key, criteria);
+};
