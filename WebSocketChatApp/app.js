5a23940c78b56993c6089d01dd65fbb70bdad6a9
FelixFanPenn <fankejin@seas.upenn.edu>
2018-01-01 00:16:56 -0500

    added a simple WebSocketChatApp

---
+++ WebSocketChatApp/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+
+app.use(express.static('public'));
+
+var server = app.listen(4000, function() {
+  console.log("listening at 4000");
+});
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('socket connection made');
+
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data);
+  });
+
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data);
+  });
+});
