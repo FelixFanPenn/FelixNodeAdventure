7d740345dbfc68d05041195d86992e7993accb44
FelixFanPenn <fankejin@seas.upenn.edu>
2017-12-31 21:19:16 -0500

    finished to-do-app

--- ToDoApp/controllers/todoController.js
+++ ToDoApp/controllers/todoController.js
@@ -1,15 +1,51 @@
-module.exports = function(app) {
 
-	app.get('/todo', function(req, res){
+var bp = require("body-parser");
+var mongoose = require("mongoose");
+var mongoURI = require("../config/keys").mongoURI;
+mongoose.Promise = global.Promise;
 
-	});
+mongoose.connect(mongoURI);
 
-	app.post('/todo', function(req, res){
-		
-	});
+var urlencodedParser = bp.urlencoded({extended: false});
 
-	app.delete('/todo', function(req, res){
-		
-	});
+var todoschema = new mongoose.Schema({
+    item: String
+});
 
+var todo = mongoose.model("Todo", todoschema);  // make a model
+
+
+module.exports = function (app) {
+
+    app.get("/todo", function (req, res) {
+        // get data from mongodb and pass it to view
+        todo.find({}, function (err, data) {
+            if (err) throw err;
+            else {
+                res.render("todo", {todos: data})
+            }
+        });  // find all items in this model
+    });
+
+    app.post("/todo", urlencodedParser, function (req, res) {
+        // get data from view and pass it to mongodb
+
+        var newTodo = todo(req.body).save(function (err, data) {
+            if (err) throw err;
+            else {
+                res.json(data);
+            }
+        });
+    });
+
+    app.delete("/todo/:item", function (req, res) {
+        // delete the requested item from mongodb
+
+        console.log(req.params.item);
+        todo.find({item: req.params.item.replace(" ", "")}).remove(function (err, data) {
+            if (err) throw err;
+            res.json(data);
+
+        })
+    });
 };
--- ToDoApp/public/assets/todo-list.js
+++ ToDoApp/public/assets/todo-list.js
@@ -4,23 +4,20 @@ $(document).ready(function(){
 
       var item = $('form input');
       var todo = {item: item.val()};
-
       $.ajax({
         type: 'POST',
         url: '/todo',
         data: todo,
         success: function(data){
-          //do something with the data via front-end framework
           location.reload();
         }
       });
-
       return false;
 
   });
 
   $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
+      var item = $(this).text();
       $.ajax({
         type: 'DELETE',
         url: '/todo/' + item,
